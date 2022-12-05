0799c5bee796a9cfad23ae88437b5048e74d0f9f
Ceynur Aydın <ceynur.aydin@gmail.com>
2022-12-05 15:01:36 +0300

    Model oluşturuldu

---
+++ RazorPizza/Model/Pizza.cs
@@ -0,0 +1,19 @@
+﻿namespace RazorPizza.Model
+{
+    public class Pizza
+    {
+        public string? Name { get; set; }
+        public string? ImageTitle { get; set; }
+        public float BasePrice { get; set; } = 10;
+        public bool Sauce { get; set; }
+        public bool Cheese { get; set; }
+        public bool Peperoni { get; set; }
+
+
+
+
+
+
+
+    }
+}
